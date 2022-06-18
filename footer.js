8588a1b99e87b2229c7221dcf6056ff265dcc28b
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-06-18 13:00:33 +0530

    footer import

--- footer.js
+++ footer.js
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./footer.css">
-    <title>Document</title>
-</head>
-<body>
-    
-
-     <!-- FOOTER -->
-     <div id="vinay-footer">
-        <!-- <main id="footer1">
+var footer = () => {
+    return `
+    <main id="footer1">
           <div class="icon">Bewakoof <span>&reg;</span></div>
           <section class="footer1-0">
             <ul class="footer1-0-div">
@@ -283,9 +271,9 @@
           </p>
   
           
-        </main> -->
-      </div>
+        </main>
+    `
+} 
 
 
-</body>
-</html>
+export default footer;
