90cfbbddd0d3af35baeda3dc5dbc36e2d47711dc
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-06-18 13:02:40 +0530

    footer added

---
+++ footer.js
@@ -0,0 +1,279 @@
+var footer = () => {
+    return `
+    <main id="footer1">
+          <div class="icon">Bewakoof <span>&reg;</span></div>
+          <section class="footer1-0">
+            <ul class="footer1-0-div">
+              <p class="footer1-0-title">COUSTMOR SERVICES</p>
+              <li>Contact Us</li>
+              <li>Track Order</li>
+              <li>Return Order</li>
+              <li>Cancel Order</li>
+            </ul>
+            <ul class="footer1-0-div">
+              <p class="footer1-0-title">COMPANY</p>
+              <li>About Us</li>
+              <li>We Are Hiring</li>
+              <li>Term & Condition</li>
+              <li>Privacy Policy</li>
+              <li>Blog</li>
+            </ul>
+            <ul class="footer1-0-div">
+              <p class="footer1-0-title">CONNECT WITH US</p>
+              <li>
+                <i class="fa-brands fa-facebook-square"></i>&nbsp;&nbsp; 4.7M
+                People Like This
+              </li>
+              <li>
+                <i class="fa-brands fa-instagram"></i>&nbsp;&nbsp; 1M Follower
+              </li>
+              <li>
+                <i class="fa-brands fa-twitter"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <i class="fa-brands fa-pinterest"></i
+                >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <i class="fa-brands fa-snapchat-square"></i
+                >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <i class="fa-brands fa-apple"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </li>
+            </ul>
+            <ul class="footer1-0-div">
+              <p class="footer1-0-title">KEEP UP TO DATE</p>
+              <form>
+                <input type="text" placeholder="Enter Email Id" id="search" />
+                <input type="submit" id="submit" value="Subscribe" />
+              </form>
+            </ul>
+          </section>
+  
+          <section class="footer1-0">
+            <ul class="footer1-0-div2">
+              <li>
+                <i class="fa-brands fa-facebook-square"></i>&nbsp;&nbsp; 4.7M
+                People Like This
+              </li>
+              <li>
+                <i class="fa-brands fa-instagram"></i>&nbsp;&nbsp; 1M Follower
+              </li>
+            </ul>
+  
+            <ul class="footer1-0-div2">
+              <p class="footer1-0-title">DOWNLOAD THE APP</p>
+              <img
+                src="https://images.bewakoof.com/web/app_android_v1.png"
+                alt=""
+              />
+              <img src="https://images.bewakoof.com/web/app_ios_v1.png" alt="" />
+            </ul>
+            <ul class="footer1-0-div2">
+              <p class="footer1-0-title">100% SECURE PAYMENT</p>
+              <img
+                src="https://images.bewakoof.com/web/secure-payments-image.png"
+                alt=""
+              />
+            </ul>
+          </section>
+        </main>
+  
+        <hr class="hr-row" />
+  
+        <main id="footer2">
+          <ul class="footer2-div">
+            <p class="footer2-div-title">MEN'S CLOTHING</p>
+            <li class="footer2-div-subTitle">Top Wear</li>
+            <li>Men's New Arrivals</li>
+            <li>Men's Half Sleeve T-Shirts</li>
+            <li>Men's Hoodies & Sweatshirts</li>
+            <li>Men's Long Sleeve T-shirts</li>
+            <li>Men's Printed T-shirts</li>
+            <li>Men's Plain T-shirts</li>
+            <li>Men's Vests</li>
+            <li>Men's Polo T-Shirts</li>
+            <li>Men's Kurtas</li>
+            <li>Men's Combo T-Shirts</li>
+            <li>Men's Shirts</li>
+            <li>Men's Nightwear</li>
+            <li>Men's Plus Size Store</li>
+  
+            <li class="footer2-div-subTitle">Bottom Wear</li>
+            <li>Men's Pajamas</li>
+            <li>Men's Boxer Shorts</li>
+            <li>Men's Shorts</li>
+            <li>Men's Track Pants</li>
+            <li>Men's Pants</li>
+  
+            <li class="footer2-div-subTitle">Featured</li>
+            <li>Men's Flip Flops</li>
+            <li>Men's Sliders</li>
+            <li>Marvel T-Shirts</li>
+            <li>Disney T-Shirts</li>
+            <li>Avengers T-Shirts</li>
+            <li>Star Wars T-Shirts</li>
+            <li>Batman T-Shirts</li>
+            <li>Superman T-Shirts</li>
+          </ul>
+  
+          <ul class="footer2-div">
+            <p class="footer2-div-title">WOMEN'S CLOTHING</p>
+            <li class="footer2-div-subTitle">Women's Top Wear</li>
+            <li>Women's New Arrivals</li>
+            <li>Women's T-Shirts</li>
+            <li>Women's Hoodies & Sweatshirts</li>
+            <li>Women's Dresses</li>
+            <li>Women's 3/4 Sleeve T-Shirts</li>
+            <li>Women's Kurtis</li>
+            <li>Women's Combo T-Shirts</li>
+            <li>Women's Nightwear</li>
+            <li>Women's Plus Size Store</li>
+  
+            <li class="footer2-div-subTitle">Women's Bottom Wear</li>
+            <li>Women's Pajamas</li>
+            <li>Women's Boxer Shorts</li>
+            <li>Women's Jeans</li>
+            <li>Women's Joggers</li>
+            <li>Women's Shorts</li>
+  
+            <li class="footer2-div-subTitle">Bags</li>
+            <li>Laptop Bags</li>
+            <li>Small Backpacks</li>
+  
+            <li class="footer2-div-subTitle">Featured</li>
+            <li>Women's Slides</li>
+            <li>Women's Flip Flops</li>
+          </ul>
+  
+          <ul class="footer2-div">
+            <p class="footer2-div-title">MOBILE COVERS</p>
+            <li class="footer2-div-subTitle">Brands</li>
+            <li>Apple</li>
+            <li>Realme</li>
+            <li>Samsung</li>
+            <li>Xiaomi</li>
+            <li>Oppo</li>
+            <li>Vivo</li>
+            <li>OnePlus</li>
+          </ul>
+  
+          <ul class="footer2-div">
+            <p class="footer2-div-title-2">FANBOOK</p>
+            <p class="footer2-div-title-2">OFFER</p>
+            <p class="footer2-div-title-2">SITE MAP</p>
+          </ul>
+        </main>
+  
+        <hr class="hr-row" />
+  
+        <main id="footer3">
+          <h2 class="footer3-title">
+            BEWAKOOF THE NEW AGE ONLINE SHOPPING EXPERIENCE.
+          </h2>
+          <p class="footer3-para">
+            Founded in 2012, Bewakoof is a lifestyle fashion brand that makes
+            creative, distinctive fashion for the trendy, contemporary Indian.
+            Bewakoof was created on the principle of creating impact through
+            innovation, honesty and thoughtfulness.
+          </p>
+  
+          <p class="footer3-para">
+            With a team of 400 members, and 2mn products sold till date. We like
+            to experiment freely, which allows us to balance creativity and
+            relatability, and our innovative designs. Our range of products is
+            always fresh and up-to-date, and we clock sales of over 1 lakh
+            products a month. Our innovation focus extends to our operations as
+            well. We are vertically integrated, manufacture our own products, and
+            cut out the middleman wherever possible. This direct-to-consumer model
+            allows us to create high-quality fashion at affordable prices. A
+            thoughtful brand, we actively attempt to minimize our environmental
+            footprint and maximize our social impact. These efforts are integrated
+            right into our day-to-day operations, from rainwater harvesting to
+            paper packaging to employee benefits. To create an accessible,
+            affordable and thoughtful experience of online shopping in India.
+          </p>
+  
+          <h2 class="footer3-title">
+            ONLINE SHOPPING AT BEWAKOOF IS HASSLE-FREE, CONVENIENT AND
+            SUPER-EXCITING!
+          </h2>
+          <p class="footer3-para">
+            Online Shopping has always been a fun and exciting task for most and
+            more so when the shopping mall is none other than your own house. We
+            have all had days when we have planned trips to the clothing store in
+            advance, dreaming about what we would buy once we get there. Now we
+            wouldnt think twice before indulging in some online shopping. Well,
+            cut to todays time and age, you can do all this from the comfort of
+            your home while enjoying many online shopping offers, right from
+            amazing deals and discounts to one of the most robust user interface
+            amongst most online shopping sites in India, with many shopping
+            filters to make your shopping experience truly hassle free. This in
+            our own words is what we call Bewakoof.com.
+          </p>
+          <p class="footer3-para">
+            Bewakoof, THE place to be when it comes to the coolest in online
+            fashion, offers you fine, high-quality merchandise go ahead and
+            indulge in a bit of online shopping for men and womens fashion. So
+            browse through the exciting categories we have on offer from mens
+            fashion to basic mens clothing as well as wide variety in womenswear
+            and womens clothes to the amazing range of accessories, fill up your
+            carts and get fast home delivery for all orders. All of this topped
+            with our exclusive online shopping offers makes for an exciting,
+            irresistible and uber cool combo! You can even gift some to your near
+            and dear ones while being absolutely certain that it will put a smile
+            on their faces.
+          </p>
+  
+          <h2 class="footer3-title">
+            BEWAKOOF.COM: THE QUIRKIEST ONLINE SHOPPING SITES OF ALL!
+          </h2>
+          <p class="footer3-para">
+            Online fashion is definitely more accessible with Bewakoof.com.
+            Explore the latest collections in Marvel t-shirts including avengers
+            t-shirts and captain America t-shirts in official merchandise. Apart
+            from these, quirkiest of T-shirts that you wont find on any online
+            shopping sites in India are showcased at Bewakoof.com. If your
+            wardrobe has been longing for a cool overhaul then bewakoof.com will
+            certainly be your best bet amongst all online shopping sites. Also,
+            take a tour of our bewakoof blog to stay abreast with the latest
+            runway trends and be a trendsetter among your immediate circles. What
+            we wear speaks volumes of us they say. But what if what you wore
+            actually spoke what your mood was! Havent we all wondered where we
+            could get those quirky t-shirts and sport them with confidence? Sure
+            otherwise getting them made or even buying them from otherwise
+            expensive online shopping sites for clothes may cost you substantially
+            but with Bewakoof.com, you will understand that you do not have to
+            spend a fortune on online fashion to look great. Sliders, joggers,
+            sweatshirts, bag and bag packs and so much more are just some of the
+            other items you can grab hold of here.
+          </p>
+  
+          <h2 class="footer3-title">
+            AVAIL OF ONLINE SHOPPING BENEFITS AT BEWAKOOF.COM AND YOULL SHOP
+            NOWHERE ELSE!
+          </h2>
+          <p class="footer3-para">
+            Choose your product, get it ordered online, and we ensure that its
+            delivery happens right at your doorstep anywhere in India. You just
+            need to take care of the payment for the product from the comfort of
+            your home, while we ensure free shipping all the time on almost
+            everything with no strings attached. For any second thoughts after
+            purchase, we have in place a return policy as well. One of the best
+            you will find on any online shopping sites in India. For your online
+            shopping experience to be safe and risk-free, we offer Cash On
+            Delivery (COD) facility too. So you dont have to worry anymore about
+            your hard earned money being stuck when you buy clothes online at
+            bewakoof.com. Avail exciting online shopping offers like designs of
+            the day and colour of the month when you shop with us. Make sure to
+            use our easy 15-day returns policy, card or cash on delivery option
+            and other customer-friendly features. A comprehensive sizing guide and
+            detailed product descriptions coupled with high-resolution product
+            shots will give you all the information to make the right buying
+            decision. Give your wardrobe the much-needed upgrade with uber cool
+            clothing head to Bewakoof.com for a great online shopping india
+            experience now! Could you have asked for more?
+          </p>
+  
+          
+        </main>
+    `
+} 
+
+
+export default footer;
